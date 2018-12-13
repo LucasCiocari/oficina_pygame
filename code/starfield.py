88365d67e6e74f33f8a5686b4ce905fb59d68888
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-12-13 12:03:24 -0200

    Desenha campo de estrelas.

---
+++ code/starfield.py
@@ -0,0 +1,20 @@
+"""Funcoes para manipulacao do campo de estrelas."""
+
+from random import choice, randrange
+
+
+def create_star(width, height):
+    """Cria uma tupla representando uma estrela."""
+    x = randrange(0, width-1)
+    y = randrange(0, height-1)
+    speed = randrange(1, 3)
+    size = randrange(1, 3)
+    color = [choice([100, 200, 250])] * 3
+    return (x, y, speed, size, color)
+
+
+def draw_star(screen, star):
+    """Desenha uma 'estrela' na tela (screen)."""
+    x, y, speed, size, color = star
+    rect = (x, y, size, size)
+    screen.fill(color, rect)
--- code/the_game.py
+++ code/the_game.py
@@ -1,15 +1,19 @@
 """Estrutura basica de um jogo com PyGame."""
 
+from starfield import create_star, draw_star
+
 import sys
 import pygame
 pygame.init()
 
 # inicializacao
-SIZE = (640, 480)
+SIZE = WIDTH, HEIGHT = (640, 480)
 FPS = 30
 fullscreen = pygame.FULLSCREEN if ("-fs" in sys.argv) else 0
 screen = pygame.display.set_mode(SIZE, fullscreen)
 running = True
+
+starfield = [create_star(WIDTH, HEIGHT) for _ in range(300)]
 
 # loop do jogo
 while running:
@@ -25,6 +29,8 @@ while running:
     # Se o jogo ainda continua...
     if running:
         # desenha objetos
+        for star in starfield:
+            draw_star(screen, star)
         pygame.display.flip()
         # manipula objetos
 
