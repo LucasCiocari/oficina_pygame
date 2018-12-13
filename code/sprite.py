dc9cd0adda4d3d6b2f5ebf83cd36a526076d2a52
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-12-13 12:49:00 -0200

    Controlando o protagonista.

--- code/sprite.py
+++ code/sprite.py
@@ -4,13 +4,31 @@ import pygame
 
 
 class Sprite(object):
-    """Define um objeto Sprite."""
+    """Define a game sprite."""
 
-    def __init__(self, image, pos):
-        """Inicializa o objeto com uma imagem e a posicao inicial."""
-        self.image = pygame.image.load(image)
+    def __init__(self, image, pos, **kwargs):
+        """Initialize a sprite object."""
         self.pos = pos
+        self.image = pygame.image.load(image)
+        self.speed = kwargs.get('speed', 1)
+        self.movement = (0, 0)
 
-    def draw(self, screen):
-        """Desenha o objeto na tela (screen)."""
+    def draw(self, screen, **kwargs):
+        """Draw the sprite to the screen."""
         screen.blit(self.image, self.pos)
+
+    def update(self):
+        """Update sprite position."""
+        x, y = self.pos
+        dx, dy = self.movement
+        speed = self.speed
+        self.pos = (x + dx*speed, y + dy*speed)
+
+    def limits(self, window):
+        """Ensure sprite in within limits."""
+        w, h = window
+        a, b = self.image.get_size()
+        x, y = self.pos
+        x = 0 if x < 0 else w-a if x > w-a else x
+        y = 0 if y < 0 else h-b if y > h-b else y
+        self.pos = (x, y)
--- code/the_game.py
+++ code/the_game.py
@@ -10,13 +10,13 @@ pygame.init()
 
 # inicializacao
 SIZE = WIDTH, HEIGHT = (640, 480)
-FPS = 30
+FPS = 60
 fullscreen = pygame.FULLSCREEN if ("-fs" in sys.argv) else 0
 screen = pygame.display.set_mode(SIZE, fullscreen)
 running = True
 clock = pygame.time.Clock()
 
-protagonista = Sprite('media/images/f18.png', (50, HEIGHT//2))
+protagonista = Sprite('media/images/f18.png', (50, HEIGHT//2), speed=3)
 
 starfield = [create_star(randrange(0, WIDTH-1), randrange(0, HEIGHT-1))
              for _ in range(300)]
@@ -32,6 +32,22 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
                 break
+            if event.key == pygame.K_LEFT:
+                dx, dy = protagonista.movement
+                dx = -1 if event.type == pygame.KEYDOWN else 0
+                protagonista.movement = (dx, dy)
+            elif event.key == pygame.K_RIGHT:
+                dx, dy = protagonista.movement
+                dx = 1 if event.type == pygame.KEYDOWN else 0
+                protagonista.movement = (dx, dy)
+            if event.key == pygame.K_UP:
+                dx, dy = protagonista.movement
+                dy = -1 if event.type == pygame.KEYDOWN else 0
+                protagonista.movement = (dx, dy)
+            elif event.key == pygame.K_DOWN:
+                dx, dy = protagonista.movement
+                dy = 1 if event.type == pygame.KEYDOWN else 0
+                protagonista.movement = (dx, dy)
     # Se o jogo ainda continua...
     if running:
         # desenha objetos
@@ -47,6 +63,7 @@ while running:
                      if star[0] > 0
                      else create_star(WIDTH-1, randrange(0, HEIGHT-1))
                      for star in starfield]
+        protagonista.update()
         # controla o tempo de atualizacao da tela.
         clock.tick(FPS)
 
